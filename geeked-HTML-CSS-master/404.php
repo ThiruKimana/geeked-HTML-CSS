b389b354a43aa559581f96239b9c5665a38d660f
kimanathiru <kimanathiru@gmail.com>
2020-05-03 19:14:26 +0300

    first commit

---
+++ geeked-HTML-CSS-master/404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Theme Name: GeekedType
+ * Theme URL: http://geekedtype.com
+ * Description: This is just the template for displaying 404 pages (not found)-- WordPress version Twenty_Sixteen
+ * Author: Thiru Kimana <kimstilltheman@geekedtype.com>
+ * Theme Version: v1.0.0
+ **/
+ 
+ 
+ get_header();
+
+?>
